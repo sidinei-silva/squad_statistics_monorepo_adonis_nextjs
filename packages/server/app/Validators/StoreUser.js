ab1625e7d0b236d21992685bd760a5799c8a0da9
Sidinei Silva <sidinei.silva02@gmail.com>
2020-08-16 02:57:50 -0300

    Incluindo mensagens customizadas no validador da api

--- packages/server/app/Validators/StoreUser.js
+++ packages/server/app/Validators/StoreUser.js
@@ -7,6 +7,18 @@ class StoreUser {
     };
   }
 
+  get messages() {
+    return {
+      'username.unique': 'Este username já está registado.',
+      'username.required': 'Você deve fornecer um username.',
+      'email.required': 'Você deve fornecer um endereço de e-mail.',
+      'email.email': 'Você deve fornecer um endereço de e-mail válido.',
+      'email.unique': 'Este e-mail já está registado.',
+      'password.required': 'Você deve fornecer uma senha',
+      'password.min': 'Senha não deve ser menor que 6 caracters',
+    };
+  }
+
   get validateAll() {
     return true;
   }
